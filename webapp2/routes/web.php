74a26d3e44da25a6d75588cba548b6c8ed69e12f
Takeru Kurihara <t.kurihara@hybrid-technologies.co.jp>
2024-06-12 19:09:23 +0900

    add register route

--- webapp2/routes/web.php
+++ webapp2/routes/web.php
@@ -13,3 +13,6 @@ Route::post('login', [AccountController::class, 'login'])->name('login');
 Route::middleware('auth')->group(function () {
     Route::get('home', [AccountController::class, 'home']);
  });
+
+Route::get('/register', 'App\Http\Controllers\Auth\RegisterController@showRegistrationForm')->name('register');
+Route::post('/register', 'App\Http\Controllers\Auth\RegisterController@register');
