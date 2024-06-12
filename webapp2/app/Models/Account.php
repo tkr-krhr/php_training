7aad5a72ad35d16b7b8b158b81234cc4638a4a99
Takeru Kurihara <t.kurihara@hybrid-technologies.co.jp>
2024-06-12 18:02:53 +0900

    create Mode

---
+++ webapp2/app/Models/Account.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Auth\Authenticatable as AuthenticableTrait;
+
+class Account extends Model implements Authenticatable
+{
+    use HasFactory;
+    use AuthenticableTrait;
+}
