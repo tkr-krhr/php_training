18c26ea0cef2be92408275171a3dfdb91be68e92
Takeru Kurihara <t.kurihara@hybrid-technologies.co.jp>
2024-06-12 18:12:29 +0900

    create web.php

--- webapp2/routes/web.php
+++ webapp2/routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\AccountController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('login', [AccountController::class, 'index']);
+Route::post('login', [AccountController::class, 'login'])->name('login');
+
+Route::middleware('auth')->group(function () {
+    Route::get('home', [AccountController::class, 'home']);
+ });
