0bf4b48dc400da87f239fb22dc065563ccefffa1
Takeru Kurihara <t.kurihara@hybrid-technologies.co.jp>
2024-06-14 11:02:06 +0900

    feat: Edit SearchController

--- webapp2/app/Http/Controllers/SearchController.php
+++ webapp2/app/Http/Controllers/SearchController.php
@@ -3,8 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class SearchController extends Controller
 {
-    //
+    // 検索フォームを表示するメソッド
+    public function showSearchForm()
+    {
+        return view('search');
+    }
+
+    // 検索ロジックを実行するメソッド
+    public function searchByEmail(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+        ]);
+
+        $email = $request->input('email');
+        $account_id = DB::table('accounts')->where('email', $email)->first(['id']);
+
+        return view('search', [
+            'email' => $email,
+            'user' => $account_id,
+        ]);
+    }
 }
